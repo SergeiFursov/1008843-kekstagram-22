3c19a13bd05f6eb38b56c3cd9641c8126cff794e
Sergei Fursov <fursov_sergei@mail.ru>
2021-02-17 18:18:59 +0500

    Больше функций

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 const getRandomInteger = function (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  // min = Math.ceil(min);
+  // max = Math.floor(max);
 
   if (min >= max) {
     return;
@@ -28,16 +28,16 @@ const getStringLength = function (checkedString, maxLineString) {
 getStringLength(123, 140);
 
 // Создание массива из 25 сгенерированных объектов
-const setPhotos = [];
-
-//Структура каждого объекта - описание фотографии
-const createPhoto = {
-  id:  '', // число — идентификатор описания.  Это число от 1 до 25. Идентификаторы не должны повторяться.
-  url: '', //строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25. Адреса картинок не должны повторяться.
-  description: '', // строка — описание фотографии.
-  likes: '', // число — количество лайков, поставленных фотографии. Случайное число от 15 до 200.
-  comments: [{Комментарий1}, {Комментарий2}, {Комментарий3}], //список комментариев, оставленных другими пользователями к этой фотографии. Количество комментариев к каждой фотографии вы определяете на своё усмотрение
-}
+// const setPhotos = [];
+//
+// //Структура каждого объекта - описание фотографии
+// const createPhoto = {
+//   id:  '', // число — идентификатор описания.  Это число от 1 до 25. Идентификаторы не должны повторяться.
+//   url: '', //строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25. Адреса картинок не должны повторяться.
+//   description: '', // строка — описание фотографии.
+//   likes: '', // число — количество лайков, поставленных фотографии. Случайное число от 15 до 200.
+//   comments: [{Комментарий1}, {Комментарий2}, {Комментарий3}], //список комментариев, оставленных другими пользователями к этой фотографии. Количество комментариев к каждой фотографии вы определяете на своё усмотрение
+// }
 
 const NAMES = [ //Набор имён для комментаторов
   'Иван',
@@ -58,10 +58,22 @@ const MESSAGES = [ //Для формирования текста коммент
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ]
 
+const SET_PHOTOS_COUNT = 25;
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomInteger(0, elements.length - 1)];
+};
+
 // Описание объекта с комментарием
-const createComment = {
-  id: '', // случайное число. Идентификаторы не должны повторяться.
-  avatar: '', // случайное число от 1 до 6
-  message: '',
-  name: '',
-}
+const createComment = () => {
+  return {
+    id: getRandomInteger(1, 25), // случайное число. Идентификаторы не должны повторяться.
+    avatar: getRandomInteger(1, 6), // случайное число от 1 до 6
+    message: getRandomArrayElement(MESSAGES),
+    name: getRandomArrayElement(NAMES),
+  };
+};
+
+const setPhotos = new Array(SET_PHOTOS_COUNT).fill(null).map(() => createComment());
+
+setPhotos
+
